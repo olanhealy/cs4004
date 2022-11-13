678c9340ea8f1144f552fc27af78d8cee2d74d00
Sean Caplis <66325420+silpac-12@users.noreply.github.com>
2022-11-13 02:46:14 +0000

    Add files via upload
    Returns an array of emails for each person in the ArrayList meetingList

---
+++ src/main/java/SendInvite.java
@@ -0,0 +1,19 @@
+
+
+public class SendInvite {
+
+    private Meeting meeting;
+
+    public SendInvite(Meeting meeting){
+        this.meeting = meeting;
+    }
+
+    public String[] sendInvite(){
+        String[] temp = new String[meeting.getMeetingList().size()];
+        for(int i = 0; i < meeting.getMeetingList().size(); i++){
+            temp[i] = meeting.getMeetingList().get(i).getEmail();
+        }
+        return temp;
+    }
+
+}
